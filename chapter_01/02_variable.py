e73054ba61ab38778a8988d489a1479a6c30be16
minghai <minghai@xhqb.com>
2026-05-31 21:21:14 +0800

    Add variable naming notes

--- chapter_01/02_variable.py
+++ chapter_01/02_variable.py
@@ -1,5 +1,7 @@
 # 变量
 # 定义：程序中用来存储单个数据的容器
+from operator import and_
+
 num = 10
 print(num)
 
@@ -13,3 +15,14 @@ print("第一个月的总播放量: ", base + incr)
 
 base, incr = 10, 20
 print("第一个月的总播放量: ", base + incr)
+
+# 变量命名规则
+# 1. 只能包含字母（a-z，A-Z）、数字（0-9）、下划线（_）
+# 2. 不能以数字开头
+# 3. 不能使用关键字：True、False、None、and、or、if、else、else、for、while
+# 4. 严格区分大小写：age、Age、AGE 是三个变量
+
+# 变量命名规范（PEP8：https://peps.python.org/pep-0008）
+#1. 见名知意
+# 2. 多个部分使用下划线连接（蛇形命名法，区别于Java的驼峰命名法）
+# 3. 英文字母全小写
