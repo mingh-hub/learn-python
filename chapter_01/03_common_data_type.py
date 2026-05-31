1cdc79434cb9f66d06e160d2a8a326159b5f94da
minghai <minghai@xhqb.com>
2026-05-31 22:11:04 +0800

    Add common data type example

---
+++ chapter_01/03_common_data_type.py
@@ -0,0 +1,27 @@
+import types
+
+# 常见数据类型
+
+# 常见基础数据类型
+# int 整数
+# float 浮点数
+# str 字符串
+# bool 布尔
+# NoneType 空值
+
+# type() 函数可以查看字面量和变量类型（变量本身没有类型，查的其实是容器内数据的类型）
+print(type(100))
+print(type("Hello"))
+a = True
+print(type(a))
+a = None
+print(type(a))
+
+# 可以通过 isinstance() 函数检查是否属于指定类型
+print(isinstance("Hello Python", str))
+print(isinstance(None, float))
+# NoneType 不是直接可用的内置名称，有如下三种写法
+print(isinstance(None, type(None)))
+print(isinstance(None, None.__class__))
+print(isinstance(None, types.NoneType)) # 这个要岛包 import types
+
