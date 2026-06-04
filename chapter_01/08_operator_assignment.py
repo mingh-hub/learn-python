f6a22f6c84391191a7d4ed7d5e143a670f89eca5
minghai <minghai@xhqb.com>
2026-06-04 23:36:59 +0800

    Add comparison operator example

--- chapter_01/08_operator_assignment.py
+++ chapter_01/08_operator_assignment.py
@@ -2,35 +2,35 @@
 
 ## 赋值运算符是编程语言中用于将值或表达式的结果保存到变量中的运算符
 
-num = int(input("input number: "))
+num = int(input("input num: "))
 
 a = num # 赋值运算符
-print(f"a is {a}")
+print(f"num is {a}")
 
 b = num
-b += 2
-print(f"b is {b}")
+b += 2 # 加法赋值运算符
+print(f"num += 2 is {b}")
 
 c = num
-c -= 2
-print(f"c is {c}")
+c -= 2 # 减法赋值运算符
+print(f"num -= 2 is {c}")
 
 d = num
-d *= 2
-print(f"d is {d}")
+d *= 2 # 乘法赋值运算符
+print(f"num *= 2 is {d}")
 
 e = num
-e /= 2
-print(f"e is {e}")
+e /= 2 # 除法赋值运算符
+print(f"num /= 2 is {e}")
 
 f = num
-f %= 2
-print(f"f is {f}")
+f %= 2 # 取模赋值运算符
+print(f"num %= 2 is {f}")
 
 g = num
-g //= 2
-print(f"g is {g}")
+g //= 2 # 取整除赋值运算符
+print(f"num //= 2 is {g}")
 
 h = num
-h **= 2
-print(f"h is {h}")
+h **= 2 # 幂赋值运算符
+print(f"num **= 2 is {h}")
