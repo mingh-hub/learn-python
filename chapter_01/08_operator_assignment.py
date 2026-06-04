82750b07db6577df8c9e230021eb3189131e6bfe
minghai <minghai@xhqb.com>
2026-06-04 23:15:51 +0800

    Add assignment operator example

---
+++ chapter_01/08_operator_assignment.py
@@ -0,0 +1,36 @@
+# 赋值运算符
+
+## 赋值运算符是编程语言中用于将值或表达式的结果保存到变量中的运算符
+
+num = int(input("input number: "))
+
+a = num # 赋值运算符
+print(f"a is {a}")
+
+b = num
+b += 2
+print(f"b is {b}")
+
+c = num
+c -= 2
+print(f"c is {c}")
+
+d = num
+d *= 2
+print(f"d is {d}")
+
+e = num
+e /= 2
+print(f"e is {e}")
+
+f = num
+f %= 2
+print(f"f is {f}")
+
+g = num
+g //= 2
+print(f"g is {g}")
+
+h = num
+h **= 2
+print(f"h is {h}")
