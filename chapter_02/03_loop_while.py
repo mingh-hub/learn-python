671acb2d76633ee0342852afaafec4845558a35b
minghai <minghai@xhqb.com>
2026-06-06 16:18:41 +0800

    Add for loop example

--- chapter_02/03_loop_while.py
+++ chapter_02/03_loop_while.py
@@ -11,6 +11,15 @@ while 条件表达式:
     循环语句2
 else:
     条件为 False，循环正常结束
+
+while 条件表达式:
+    循环语句1
+    if True :  # 条件判断是否执行 循环语句2
+        循环语句2
+        break # 跳出循环，循环语句3 及后面的 else 语句不会执行
+    循环语句3
+else:
+    条件为 False，循环正常结束
 """
 count = 0
 while count < 5:
---
+++ chapter_02/04_loop_for.py
@@ -0,0 +1,37 @@
+# for 循环
+"""
+for 循环和 while 循环区别：
+1. while 循环是通过条件表达式来控制是否要进行下一次循环，而 for 循环的本质是一种轮询遍历机制，对集合内容进行逐条处理
+2. 都可以用 break 强制中断循环
+"""
+
+## 语法结构
+"""
+for element in collection
+    loop code
+else: # 可选结构，可有可无，是在 for 循环结束后执行的语句
+    loop end code
+"""
+
+msg = "Hello Python"
+for char in msg:
+    print(char)
+    break
+
+## range 语句
+"""
+作用：生成指定队则的数字序列
+用法：
+1. range(end)：获取一个从 0 开始到 end 结束的数字序列（不含 end）
+2. range(start, end)：获取一个从 start 开始到 end 结束的数字序列（不含 end）
+3. range(start, end, step)：获取一个从 start 开始到 end 结束，step 步长的数字序列（不含 end）
+"""
+
+## 案例：计算 100-500 之间所有 3 的倍数的数字和
+coll = range(100, 501)
+add = 0
+for i in coll:
+    if i % 3 == 0:
+        add += i
+else:
+    print(f"100-500 之间所有 3 的倍数的数字和为：{add}")
