e11726c60ec0598797603de925459ba50b8c1dc9
minghai <minghai@xhqb.com>
2026-06-13 18:36:10 +0800

    Refine list merge headings

--- chapter_03/14_data_list_merge.py
+++ chapter_03/14_data_list_merge.py
@@ -3,7 +3,7 @@
 num_list1 = [19, 23, 54, 64, 875, 20, 109, 232, 12, 23, 54]
 num_list2 = [55, 80, 72, 35, 60, 123, 54, 29, 91]
 
-## 1. 正常合并
+## 1. 常规合并
 for num in num_list2:
     num_list1.append(num)
 print(f"正常合并后的列表：{num_list1}")
@@ -39,7 +39,7 @@ for num in num_list:
         new_list.append(num)
 print(f"使用 + 合并去重后的列表：{new_list}")
 
-## 使用列表推导式
+## 4. 使用列表推导式
 seen = set()
 print(seen)
 new_list = [x for x in num_list if x not in seen and not seen.add(x)] # seen.add(x) 如果 seen 已存在，返回 None，not seen.add(x) 表示向集合 seen 中添加元素成功
